9649a9059d4fc87314d037eef8c0646d614372a7
Pavlos Psomiadis <psom.pavos@gmail.com>
2025-12-07 13:55:01 +0200

    fix: add new print hello coding

--- src/gr/aueb/cf/testgit/SayHelloMain.java
+++ src/gr/aueb/cf/testgit/SayHelloMain.java
@@ -3,5 +3,7 @@ package gr.aueb.cf.testgit;
 public class SayHelloMain {
     public static void main(String[] args) {
         System.out.println("Hello Everybody");
+
+        System.out.println("Hello Coding");
     }
 }
